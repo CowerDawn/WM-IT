f6e616ec6db67c6f2e2587a4b78ca5ed1b3c6cbe
CowerDawn <bigdickpopboba@gmail.com>
2025-02-21 12:56:05 +0300

    Update logo.c

--- vi/logo.c
+++ vi/logo.c
@@ -1,11 +1,1 @@
-#include <gtk/gtk.h>
-#include <vte/vte.h>
-#include <gdk/gdkkeysyms.h>
-#include <glib/gstdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-typedef struct {
-    Theme current_theme;
-    gdouble font_scale;
-} Config;
